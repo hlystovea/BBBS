8a8b384fc78911dc7f09a2298a8625dad252297b
Evgenii Khlystov <hlystovea@gmail.com>
2021-09-18 16:48:58 +0700

    Startapp afisha

--- project/afisha/models.py
+++ project/afisha/models.py
@@ -1,24 +1,29 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
+
+User = get_user_model()
 
 
 class EventMailing(models.Model):
     event = models.ForeignKey(
         'api.Event',
         verbose_name=_('Событие'),
+        related_name='mailings',
         on_delete=models.CASCADE,
     )
     user = models.ForeignKey(
-        'api.Event',
+        User,
+        verbose_name=_('Пользователь'),
+        related_name='event_mailings',
         on_delete=models.CASCADE,
-        verbose_name=_('Пользователь')
     )
     date_sending = models.DateTimeField(
         verbose_name=_('Время отправки'),
         auto_now_add=True,
     )
     mailing_type = models.CharField(
-        verbose_name=_('Тип отправки'),
+        verbose_name=_('Тип сообщения'),
         max_length=12,
         choices=(
             ('cancellation', _('Отмена')),
@@ -28,5 +33,5 @@ class EventMailing(models.Model):
 
     class Meta:
         ordering = ('-date_sending',)
-        verbose_name = _('Рассылка отмены события')
-        verbose_name_plural = _('Рассылки отмены событий')
+        verbose_name = _('Рассылка уведомлений')
+        verbose_name_plural = _('Рассылки уведомлений')
--- project/project/settings.py
+++ project/project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django_filters',
     'corsheaders',
     'drf_yasg',
+    'afisha',
     'api',
     'account',
     'common',
