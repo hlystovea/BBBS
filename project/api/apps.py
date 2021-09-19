d5320b226edde32fff9b9d6c41c840b2f16e00c2
Evgenii Khlystov <hlystovea@gmail.com>
2021-09-19 16:47:50 +0700

    The verbose_name of apps has been changed

--- project/api/apps.py
+++ project/api/apps.py
@@ -5,4 +5,4 @@ from django.utils.translation import gettext_lazy as _
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
-    verbose_name = _('Страницы сайта')
+    verbose_name = _('Контент сайта')
