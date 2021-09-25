f64f46740ff163bcfbb0d59a67b3b836eeb3ce1b
Evgenii Khlystov <hlystovea@gmail.com>
2021-09-25 16:55:31 +0700

    Added has_curator field to the DiarySerializer

--- project/api/views/diary.py
+++ project/api/views/diary.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.core.mail import send_mail
+from django.db.models import Case, When
 from django.utils.translation import gettext_lazy as _
 from requests.exceptions import RequestException
 from rest_framework import permissions, status, viewsets
@@ -19,7 +20,16 @@ class DiaryViewSet(viewsets.ModelViewSet):
     pagination_class = LimitOffsetPagination
 
     def get_queryset(self):
-        return Diary.objects.filter(mentor=self.request.user).order_by('-date')
+        return Diary.objects.filter(
+            mentor=self.request.user
+        ).annotate(
+            has_curator=Case(
+                When(mentor__curator_id=None, then=False),
+                default=True
+            )
+        ).order_by(
+            '-date'
+        )
 
     def perform_create(self, serializer):
         serializer.save(mentor=self.request.user)
@@ -28,33 +38,38 @@ class DiaryViewSet(viewsets.ModelViewSet):
     def send(self, request, pk=None):
         mentor = request.user
         diary = get_object_or_404(Diary, pk=pk, mentor=mentor)
-        if not diary.sent_to_curator:
-            email = mentor.curator.email
-            serializer = EmailSerializer(data={'email': email})
-            if serializer.is_valid(raise_exception=True):
-                try:
-                    send_mail(
-                        subject=settings.SEND_DIARY_TO_CURATOR_SUBJECT % (
-                            mentor,
-                            diary.date.isoformat()
-                        ),
-                        message=diary.description,
-                        from_email=None,
-                        recipient_list=[email],
-                    )
-                except RequestException:
-                    message = {
-                        'diary': _('Проблемы с отправкой, попробуйте позднее.')
-                    }
-                    return Response(
-                        message,
-                        status=status.HTTP_504_GATEWAY_TIMEOUT
-                    )
-                diary.sent_to_curator = True
-                diary.save()
-                message = {'diary': _('Запись дневника отправлена куратору.')}
-                return Response(message, status=status.HTTP_200_OK)
-        message = {
-            'diary': _('Запись дневника уже была отправлена куратору ранее.')
-        }
-        return Response(message, status=status.HTTP_200_OK)
+        if not mentor.curator:
+            message = {'diary': _('У вас нет куратора.')}
+            return Response(message, status=status.HTTP_200_OK)
+        if diary.sent_to_curator:
+            message = {
+                'diary': _(
+                    'Запись дневника уже была отправлена куратору ранее.'
+                )
+            }
+            return Response(message, status=status.HTTP_200_OK)
+        email = mentor.curator.email
+        serializer = EmailSerializer(data={'email': email})
+        if serializer.is_valid(raise_exception=True):
+            try:
+                send_mail(
+                    subject=settings.SEND_DIARY_TO_CURATOR_SUBJECT % (
+                        mentor,
+                        diary.date.isoformat()
+                    ),
+                    message=diary.description,
+                    from_email=None,
+                    recipient_list=[email],
+                )
+            except RequestException:
+                message = {
+                    'diary': _('Проблемы с отправкой, попробуйте позднее.')
+                }
+                return Response(
+                    message,
+                    status=status.HTTP_504_GATEWAY_TIMEOUT
+                )
+            diary.sent_to_curator = True
+            diary.save()
+            message = {'diary': _('Запись дневника отправлена куратору.')}
+            return Response(message, status=status.HTTP_200_OK)
